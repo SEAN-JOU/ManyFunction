600a4d2bf898cac5461f4f2e7271bd182c60a515
Jou-Sean <present810209@gmail.com>
2017-10-18 21:28:28 +0800

    ww

--- app/src/main/java/com/example/jou/manyfunction/Main3Activity.java
+++ app/src/main/java/com/example/jou/manyfunction/Main3Activity.java
@@ -1,17 +1,34 @@
 package com.example.jou.manyfunction;
 
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TabHost;
 
 public class Main3Activity extends  AppCompatActivity {
     TabHost mTabHost;
+    ProgressBar pbp;
+    ProgressDialog pd;
+    RadioGroup r1;
+    RadioButton s1,s2;
+    Button execute1,dialogppp,shine1,button4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        r1=(RadioGroup)findViewById(R.id.r1);
+        s1=(RadioButton)findViewById(R.id.s1);
+        s2=(RadioButton)findViewById(R.id.s2);
 
         mTabHost = (TabHost)findViewById(R.id.tabHost);
         mTabHost.setup();
@@ -29,7 +46,94 @@ public class Main3Activity extends  AppCompatActivity {
         mTabHost.addTab(mTabHost.newTabSpec("2")
                 .setIndicator("蓝色")
                 .setContent(R.id.tab3));
+
+        execute1=(Button)findViewById(R.id.execute1);
+        dialogppp=(Button)findViewById(R.id.dialogppp);
+        shine1=(Button)findViewById(R.id.shine1);
+        pbp =(ProgressBar)findViewById(R.id.ppp);
+        button4=(Button)findViewById(R.id.button4);
+
+        execute1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pbp.setVisibility(view.getVisibility()/*取得顯示狀態*/);/*設定顯示狀態*/
+
+            }});
+
+
+        shine1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                pbp.setVisibility(View.VISIBLE/*畫面顯示*/);
+
+                new Thread(){
+                    @Override
+                    public void run()
+                    {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }/*新增匿名 分支執行續*/
+
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                pbp.setVisibility(View.INVISIBLE/*畫面隱藏*/);
+                            }});}}.start();}});
+
+        dialogppp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                pd = new ProgressDialog(Main3Activity.this);
+                pd.setMessage("Please Wait");
+                pd.setCancelable(false);
+                pd.show();
+                new Thread() {
+                    @Override
+                    public void run()
+                    {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                pd.dismiss();
+                            }});}}.start();
+
+            }});
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pbp.setVisibility(View.INVISIBLE);
+            }});
+
+        r1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged/*當檢查改變的時候*/(RadioGroup radioGroup, @IdRes int i) {
+
+                switch(r1.getCheckedRadioButtonId()/*取得檢查RadioButtonId*/) {
+                    case R.id.s1:
+                        s1.setBackgroundColor(Color.GREEN);
+                        s2.setBackgroundColor(Color.WHITE);
+                        break;
+                    case R.id.s2:
+                        s2.setBackgroundColor(Color.RED);
+                        s1.setBackgroundColor(Color.WHITE);
+                        break;}
+
+
+            }});
+
     }}
+
+
 
 
 
